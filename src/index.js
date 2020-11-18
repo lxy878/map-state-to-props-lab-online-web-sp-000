fde5c7a925e28be7ce24d7fcb0b2310176854811
Xiaoyi Liu <lxy878@gmail.com>
2020-11-18 14:11:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import manageUsers from './reducers/manageUsers'
 
 
